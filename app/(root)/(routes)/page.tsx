35e04ddfc87f1c319e53fad7aa8224401b95d62e
Matheus Paulo <matheusslg1@gmail.com>
2023-08-18 20:47:32 -0300

    feat: Form components

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -16,11 +16,7 @@ const SetupPage = () => {
     }
   }, [isOpen, onOpen]);
 
-  return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/"/>
-    </div>
-  );
+  return null
 }
 
 export default SetupPage
